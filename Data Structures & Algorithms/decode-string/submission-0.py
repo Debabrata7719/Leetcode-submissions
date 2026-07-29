803b202348d61f23540c86b093ec62a3622ecdc1
Debabrata Dey <debabratadey8080@gmail.com>
2026-07-29 08:29:32 +0530

    Add: decode-string - submission-0

---
+++ Data Structures & Algorithms/decode-string/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+      
+
+        stack = []
+
+        for ch in s:
+            if ch != "]":
+                stack.append(ch)
+            else:
+                # string inside []
+                curr = ""
+                while stack[-1] != "[":
+                    curr = stack.pop() + curr
+
+                stack.pop()  # remove '['
+
+                # number before '['
+                num = ""
+                while stack and stack[-1].isdigit():
+                    num = stack.pop() + num
+
+                stack.append(curr * int(num))
+
+        return("".join(stack))
+        
