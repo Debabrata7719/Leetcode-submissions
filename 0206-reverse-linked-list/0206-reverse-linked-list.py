061cca523384f5bd41ce01ce7d4fb8a495680783
Debabrata Dey <debabratadey8080@gmail.com>
2026-09-02 10:02:12 +0530

    Time: 0 ms (100%), Space: 14.2 MB (82.47%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        previous = None
+        current = head
+
+        while current is not None:
+            next_int = current.next
+
+            current.next= previous
+
+            previous= current
+
+            current = next_int
+
+        return previous
+
+            
+
+
+       
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
