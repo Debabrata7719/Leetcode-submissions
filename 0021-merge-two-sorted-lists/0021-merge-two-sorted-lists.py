06e1526e418eb15b147178df1e4543fe3ac6fa5f
Debabrata Dey <debabratadey8080@gmail.com>
2026-09-02 10:50:31 +0530

    Time: 0 ms (100%), Space: 12.5 MB (47.94%) - LeetHub - Time: O(n + m), Space: O(1)

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+
+        dummy = ListNode(0)
+        current = dummy
+
+        while list1 is not None and list2 is not None:
+
+            if list1.val <= list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+
+            current = current.next
+
+        if list1 is not None:
+            current.next = list1
+        else:
+            current.next = list2
+
+        return dummy.next
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
